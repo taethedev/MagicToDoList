b097a8fd0b3afe3e61d7d1515b130ec6ad127803
Tae Jung <kwang8821@gmail.com>
2022-06-30 23:06:15 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ToDoList
+-JS/HTML/CSS
--- todo.js
+++ todo.js
@@ -1,29 +1,73 @@
-function addToList() {
-    const todoInput = document.getElementById("todoInput");
-    const todo = todoInput.value;
-    if(todo) {
-        const todoList = document.getElementById("todoList");    
+
+var currentList = [];
+const input = document.getElementById('todoInput');
+let addButton = document.getElementById("addTodo");
+addButton.addEventListener("click", ()=> addToList(input.value))
+const todoList = document.getElementById("todoList");    
+const deleteBtn = document.getElementById('deleteAll');
+deleteBtn.addEventListener('click', ()=> deleteAll())
+
+
+input.focus();
+input.addEventListener('keypress', (e)=>{
+    if(e.key === 'Enter') {
+        addToList(input.value);
+    }
+})
+getLocalStorage();
+createTodoList();
+
+function addToList(value, init) {
+    if(value) {
         const li = document.createElement('li');
         const deleteBtn = document.createElement('button');
         deleteBtn.classList.add('delete');
         deleteBtn.textContent = 'X';
-        deleteBtn.addEventListener('click', ()=>{li.remove()})
-        li.textContent = todo;
+        deleteBtn.addEventListener('click', ()=>{
+            removeFromList(li);
+        })
+        li.textContent = value;
         li.appendChild(deleteBtn);
         li.classList.add('todoItem');
         todoList.appendChild(li);
         todoInput.value = '';
         todoInput.focus();
+        
+        if(!init) {
+            currentList.push(value)
+            setLocalStorage();
+        }
+       
     }
-    
 }
-let addButton = document.getElementById("addTodo");
-addButton.addEventListener("click", ()=> addToList())
-
-const input = document.getElementById('todoInput');
-input.focus();
-input.addEventListener('keypress', (e)=>{
-    if(e.key === 'Enter') {
-        addToList();
+function removeFromList(element) {
+    if(element instanceof HTMLElement) {
+        let content = element.firstChild.textContent;
+        currentList.splice(currentList.indexOf(content), 1);
+        element.remove();
+        setLocalStorage();
     }
-})
+}
+function setLocalStorage(){
+    localStorage.clear();
+    localStorage.setItem('todoList', currentList);
+}
+function getLocalStorage(){
+    currentList = localStorage.getItem('todoList') ? localStorage.getItem('todoList') : [];
+    currentList = currentList.length ? currentList.split(',') : [];
+}
+function createTodoList() {
+    if(currentList && currentList.length) {
+        currentList.forEach((element)=>{
+            addToList(element, true);
+        })
+    }
+}
+function deleteAll() {
+    currentList = [];
+    localStorage.clear();
+    
+    while (todoList.firstChild) {
+        todoList.removeChild(todoList.firstChild);
+    }
+}
